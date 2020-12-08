ff8486e5e9917aa2a5a5f3e334fadecf689c8eef
 Evgeniy <evgeniy@MacBook-Pro-Evgeniy.local>
2020-12-08 13:58:25 +0300

    parcer

---
+++ parce.go
@@ -0,0 +1,220 @@
+package testparcer
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrorWhileReadingFile     = errors.New("error while reading file")
+	ErrorWhileUnmarshaling    = errors.New("error while unmarshaling")
+	ErrorWhileChekingRequired = errors.New("error while cheking requiered fields")
+	ErrorWhileSettingDefault  = errors.New("error while setting fields")
+)
+
+// Parce принимает путь файла и с труктуру в которю распарсит json, возвращает ошибку
+func Parce(filepath string, target interface{}) error {
+	bts, err := readJSON(filepath)
+	if err != nil {
+		err := fmt.Errorf("%w: %v", ErrorWhileReadingFile, err)
+		return err
+	}
+
+	err = json.Unmarshal(bts, target)
+	if err != nil {
+		err := fmt.Errorf("%w: %v", ErrorWhileUnmarshaling, err)
+		return err
+	}
+
+	err = checkeRequiredFields(target)
+	if err != nil {
+		err := fmt.Errorf("%w: %v", ErrorWhileUnmarshaling, err)
+		return err
+	}
+
+	err = setDefaultFields(target)
+	if err != nil {
+		err := fmt.Errorf("%w: %v", ErrorWhileUnmarshaling, err)
+		return err
+	}
+
+	return nil
+}
+
+func checkeRequiredFields(target interface{}) error {
+	fields := reflect.ValueOf(target).Elem()
+
+	for i := 0; i < fields.NumField(); i++ {
+		tagStr := fields.Type().Field(i).Tag.Get("json")
+
+		if strings.Contains(tagStr, "required") && fields.Field(i).IsZero() {
+			err := fmt.Sprintf(`required field "%v" (tag "%v") is missing`, fields.Type().Field(i).Name, strings.Split(tagStr, ",")[0])
+			return errors.New(err)
+		}
+
+		switch fields.Field(i).Kind() {
+		case reflect.Struct:
+			err := checkeRequiredFields(fields.Field(i).Addr().Interface())
+			if err != nil {
+				return err
+			}
+		case reflect.Map:
+			for _, key := range fields.Field(i).MapKeys() {
+				if (fields.Field(i).MapIndex(key).Kind() == reflect.Struct ||
+					fields.Field(i).MapIndex(key).Kind() == reflect.Ptr) && !fields.Field(i).MapIndex(key).IsZero() {
+					err := checkeRequiredFields(fields.Field(i).MapIndex(key).Interface())
+					if err != nil {
+						return err
+					}
+				}
+			}
+		case reflect.Slice:
+			for j := 0; j < fields.Field(i).Len(); j++ {
+				if (fields.Field(i).Index(j).Kind() == reflect.Struct ||
+					fields.Field(i).Index(j).Kind() == reflect.Ptr) && !fields.Field(i).Index(j).IsZero() {
+					err := checkeRequiredFields(fields.Field(i).Index(j).Addr().Interface())
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func setDefaultFields(target interface{}) error {
+	fields := reflect.ValueOf(target).Elem()
+
+	for i := 0; i < fields.NumField(); i++ {
+		tagStr := fields.Type().Field(i).Tag.Get("default")
+
+		if tagStr != "" && fields.Field(i).IsZero() {
+			switch fields.Field(i).Kind() {
+			case reflect.Int:
+				val, err := strconv.ParseInt(tagStr, 10, 32)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetInt(val)
+			case reflect.Int8:
+				val, err := strconv.ParseInt(tagStr, 10, 8)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetInt(val)
+			case reflect.Int16:
+				val, err := strconv.ParseInt(tagStr, 10, 16)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetInt(val)
+			case reflect.Int32:
+				val, err := strconv.ParseInt(tagStr, 10, 32)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetInt(val)
+			case reflect.Int64:
+				val, err := strconv.ParseInt(tagStr, 10, 64)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetInt(val)
+			case reflect.Uint:
+				u, err := strconv.ParseUint(tagStr, 10, 32)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetUint(u)
+			case reflect.Uint8:
+				u, err := strconv.ParseUint(tagStr, 10, 8)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetUint(u)
+			case reflect.Uint16:
+				u, err := strconv.ParseUint(tagStr, 10, 16)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetUint(u)
+			case reflect.Uint32:
+				u, err := strconv.ParseUint(tagStr, 10, 32)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetUint(u)
+			case reflect.Uint64:
+				u, err := strconv.ParseUint(tagStr, 10, 64)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetUint(u)
+			case reflect.Float32:
+				f, err := strconv.ParseFloat(tagStr, 32)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetFloat(f)
+			case reflect.Float64:
+				f, err := strconv.ParseFloat(tagStr, 64)
+				if err != nil {
+					return err
+				}
+				fields.Field(i).SetFloat(f)
+			case reflect.String:
+				fields.Field(i).SetString(tagStr)
+			default:
+				err := fmt.Sprintf(`type of field "%v" (type %v) is not support setting defaul value`, fields.Type().Field(i).Name, fields.Type().Field(i).Type)
+				return errors.New(err)
+			}
+		}
+
+		switch fields.Field(i).Kind() {
+		case reflect.Struct:
+			err := setDefaultFields(fields.Field(i).Addr().Interface())
+			if err != nil {
+				return err
+			}
+		case reflect.Slice:
+			for j := 0; j < fields.Field(i).Len(); j++ {
+				if (fields.Field(i).Index(j).Kind() == reflect.Struct ||
+					fields.Field(i).Index(j).Kind() == reflect.Ptr) && !fields.Field(i).Index(j).IsZero() {
+					err := setDefaultFields(fields.Field(i).Index(j).Addr().Interface())
+					if err != nil {
+						return err
+					}
+				}
+			}
+		case reflect.Map:
+			for _, key := range fields.Field(i).MapKeys() {
+				if (fields.Field(i).MapIndex(key).Kind() == reflect.Struct ||
+					fields.Field(i).MapIndex(key).Kind() == reflect.Ptr) && !fields.Field(i).MapIndex(key).IsZero() {
+					err := setDefaultFields(fields.Field(i).MapIndex(key).Interface())
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func readJSON(filepath string) ([]byte, error) {
+	bts, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	return bts, nil
+}
---
+++ parce_test.go
@@ -0,0 +1,359 @@
+package testparcer
+
+import (
+	"testing"
+)
+
+type testDefaultStruct struct {
+	Name string `json:"name,required"`
+	Age  int    `json:"age" default:"18"`
+}
+
+type testStructWithNested struct {
+	Name   string            `json:"name,required"`
+	Age    int               `json:"age" default:"18"`
+	Parent testDefaultStruct `json:"parent,required"`
+}
+
+type testStructWithNestedMapOfPointers struct {
+	Name   string                        `json:"name,required"`
+	Age    int                           `json:"age" default:"18"`
+	Parent map[string]*testDefaultStruct `json:"parent,required"`
+}
+
+type testStructWithNestedArray struct {
+	Name   string              `json:"name,required"`
+	Age    int                 `json:"age" default:"18"`
+	Parent []testDefaultStruct `json:"parent,required"`
+}
+
+type testStructWithNestedMap struct {
+	Name   string            `json:"name,required"`
+	Age    int               `json:"age" default:"18"`
+	Parent map[string]string `json:"parent,required"`
+}
+
+func Test_checkeRequiredFields(t *testing.T) {
+	type args struct {
+		target interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"test_1",
+			args{
+				&testDefaultStruct{
+					Name: "Jin",
+				},
+			},
+			false,
+		},
+		{
+			"test_2",
+			args{
+				&testDefaultStruct{},
+			},
+			true,
+		},
+		{
+			"test_3",
+			args{
+				&testStructWithNested{
+					Name: "jin",
+					Parent: testDefaultStruct{
+						Name: "pa",
+					},
+				},
+			},
+			false,
+		},
+		{
+			"test_4",
+			args{
+				&testStructWithNested{
+					Name:   "jin",
+					Parent: testDefaultStruct{},
+				},
+			},
+			true,
+		},
+		{
+			"test_5",
+			args{
+				&testStructWithNested{
+					Name: "jin",
+				},
+			},
+			true,
+		},
+		{
+			"test_6",
+			args{
+				&testStructWithNestedMapOfPointers{
+					Name: "jin",
+					Parent: map[string]*testDefaultStruct{
+						"mam": &testDefaultStruct{Name: "helen"},
+						"pa":  &testDefaultStruct{Name: "jo"},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"test_7",
+			args{
+				&testStructWithNestedMapOfPointers{
+					Name: "jin",
+					Parent: map[string]*testDefaultStruct{
+						"mam": &testDefaultStruct{Name: "helen"},
+						"pa":  &testDefaultStruct{},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"test_8",
+			args{
+				&testStructWithNestedArray{
+					Name: "jin",
+					Parent: []testDefaultStruct{
+						testDefaultStruct{Name: "helen"},
+						testDefaultStruct{Name: "jo"},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"test_9",
+			args{
+				&testStructWithNestedArray{
+					Name: "jin",
+					Parent: []testDefaultStruct{
+						testDefaultStruct{Name: "helen"},
+						testDefaultStruct{Age: 20},
+					},
+				},
+			},
+			true,
+		},
+		{
+			"test_10",
+			args{
+				&testStructWithNestedMap{
+					Name: "jin",
+					Parent: map[string]string{
+						"mam": "Lisa",
+						"pa":  "jo",
+					},
+				},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := checkeRequiredFields(tt.args.target); (err != nil) != tt.wantErr {
+				t.Errorf("checkeRequiredFields() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+type testStructDefaultFields struct {
+	F1 int8   `default:"127"`
+	F2 string `default:"str"`
+	F3 byte   `default:"16"`
+}
+
+type testStructDefaultFieldsWrongInt struct {
+	F1 int    `default:"10a"`
+	F2 string `default:"str"`
+	F3 byte   `default:"16"`
+}
+
+type testStructDefaultFieldsWrongByte struct {
+	F1 int    `default:"10"`
+	F2 string `default:"str"`
+	F3 byte   `default:"16a"`
+}
+
+type testStructDefaultFieldsWrongArr struct {
+	F1 []int `default:"10"`
+}
+
+type testStructDefaultFieldsWrongMap struct {
+	F1 map[string]string `default:"10"`
+}
+
+type testStructDefaultFieldsWrongStruct struct {
+	F1 testStructDefaultFields `default:"10"`
+}
+
+type testStructDefaultFieldsStruct struct {
+	F1 testStructDefaultFields
+}
+
+type testStructDefaultFieldsMap struct {
+	F1 map[string]*testStructDefaultFields
+}
+
+func Test_setDefaultFields(t *testing.T) {
+	type args struct {
+		target interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"test_1",
+			args{
+				&testStructDefaultFields{},
+			},
+			false,
+		},
+		{
+			"test_2",
+			args{
+				&testStructDefaultFieldsWrongInt{},
+			},
+			true,
+		},
+		{
+			"test_3",
+			args{
+				&testStructDefaultFieldsWrongByte{},
+			},
+			true,
+		},
+		{
+			"test_4",
+			args{
+				&testStructDefaultFieldsWrongArr{},
+			},
+			true,
+		},
+		{
+			"test_5",
+			args{
+				&testStructDefaultFieldsWrongMap{},
+			},
+			true,
+		},
+		{
+			"test_6",
+			args{
+				&testStructDefaultFieldsWrongStruct{},
+			},
+			true,
+		},
+		{
+			"test_7",
+			args{
+				&testStructDefaultFieldsStruct{},
+			},
+			false,
+		},
+		{
+			"test_8",
+			args{
+				&testStructDefaultFieldsMap{
+					F1: map[string]*testStructDefaultFields{
+						"foo": &testStructDefaultFields{},
+						"bar": &testStructDefaultFields{},
+					},
+				},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := setDefaultFields(tt.args.target); (err != nil) != tt.wantErr {
+				t.Errorf("setDefaultFields() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+type testStruct struct {
+	F1 byte                           `json:"byte-field"`                           // обычное поле без новых тэгов; опциональное, так как нет ключа required
+	F2 string                         `json:"string-field,required"`                // строковое поле; обязательное, так как есть ключ required
+	F3 int                            `json:"int1-field" default:"123"`             // числовое поле со значением по умолчанию 123
+	F4 int                            `json:"int2-field" default:"foo"`             // числовое поле со значением по умолчанию foo (ошибка, некорректное значение)
+	F5 []string                       `json:"slice-field,required" default:"what?"` // обязательное поле со значением по умолчанию (ошибка, срезы не могут иметь default)
+	F6 anotherTestStruct1             `json:"struct-field,required"`                // подструктура, которая тоже может иметь тэги и т.д.
+	F7 map[string]int                 `json:"primitive-map-field"`                  // обычное отображение без новых тэгов
+	F8 map[string]*anotherTestStruct2 `json:"struct-map-field,required"`            // отображение на указатели подструктур, которые тоже могут иметь тэги и т.д.
+}
+
+type anotherTestStruct1 struct {
+	F1 byte   `json:"byte-field"`            // обычное поле без новых тэгов; опциональное, так как нет ключа required
+	F2 string `json:"string-field,required"` // строковое поле; обязательное, так как есть ключ required
+	F3 int    `json:"int1-field" default:"123"`
+}
+
+type anotherTestStruct2 struct {
+	F1 byte   `json:"byte-field"`            // обычное поле без новых тэгов; опциональное, так как нет ключа required
+	F2 string `json:"string-field,required"` // строковое поле; обязательное, так как есть ключ required
+	F3 int    `json:"int1-field" default:"123"`
+}
+
+func TestParce(t *testing.T) {
+	type args struct {
+		filepath string
+		target   interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"test_1 string into int",
+			args{
+				filepath: "test1.json",
+				target:   &testStruct{},
+			},
+			true,
+		},
+		{
+			"test_2 required field F5",
+			args{
+				filepath: "test2.json",
+				target:   &testStruct{},
+			},
+			true,
+		},
+		{
+			"test_3",
+			args{
+				filepath: "test3.json",
+				target:   &testStruct{},
+			},
+			false,
+		},
+		{
+			"test_4 F4 wrong default",
+			args{
+				filepath: "test4.json",
+				target:   &testStruct{},
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Parce(tt.args.filepath, tt.args.target)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parce() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
